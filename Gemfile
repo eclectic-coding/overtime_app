df267cd0fd311566cafb8ea0b32dac1f80c260ac
Chuck Smith <chuck@eclecticsaddlebag.com>
2020-08-24 20:43:22 -0400

    Implement administrate dashboard

--- Gemfile
+++ Gemfile
@@ -2,6 +2,7 @@ source "https://rubygems.org"
 
 ruby "2.7.1"
 
+gem "administrate"
 gem "bootsnap", ">= 1.4.2", require: false
 gem "devise"
 gem "devise-bootstrapped"
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
-  devise_for :users
+  namespace :admin do
+      resources :users
+      resources :admin_users
+      resources :posts
+
+      root to: "users#index"
+    end
+  devise_for :users, skip: [:registration]
 
   root "static#home"
 
